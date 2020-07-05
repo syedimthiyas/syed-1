3703c9fba28aa755843406b747365b84e344ca4f
Syed Imthiyas <syed.gmail.com>
2020-07-05 18:05:17 +0530

    Added READ.MD, Added Comment Lines

--- Hello.java
+++ Hello.java
@@ -1,3 +1,5 @@
+// This is Java program will print a sample message
+
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
---
+++ READ.md
@@ -0,0 +1,6 @@
+This java code will print some messages
+
+
+Dev:
+Gopi
+Shankar
