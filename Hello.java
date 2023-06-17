5000fd6d8f8472ca2f5f570b7ef1dc31ef969686
syed Imthiyas <syedimthiyas4all@gmail.com>
2023-06-16 21:15:41 -0400

    added quizar

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
      System.out.println("Hello Syed");
+     System.out.println("Hello quizar");
      System.out.println("Hello Syed");
     }  
 } 
